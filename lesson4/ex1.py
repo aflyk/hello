f7f8f1365a01b7ff82f67d92f10358d6e602d821
Alexey Reukov <friken@list.ru>
2020-01-02 21:35:13 +1100

    4 урок 1 задание

--- lesson4/ex1.py
+++ lesson4/ex1.py
@@ -1,6 +1,7 @@
-my_list1 = [1,2,1,12,20,13]
+my_list1 = [1, 2, 1, 12, 20, 13]
 my_list2 = [1, 7, 12, 576]
 for i in my_list2:
     if i in my_list1:
         for j in range(my_list1.count(i)):
             my_list1.remove(i)
+print(my_list1)
