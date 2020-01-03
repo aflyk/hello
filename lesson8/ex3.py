64ae6b0e7307d58dbc340c75bf6e71767b852269
Alexey Reukov <friken@list.ru>
2020-01-03 21:45:10 +1100

    add ex3-2

--- lesson8/ex3.py
+++ lesson8/ex3.py
@@ -1,18 +1,21 @@
 def attack(avatar, target):
-    target['health'] = target['health'] - avatar['damage']
+    target['health'] = target['health'] - atk(avatar['damage'], target["armor"])
 
-
+def atk(damage, armor):
+    return damage/armor
 
 
 player = {
     "name" : input("Введите имя вашего персонажа: "),
     "health" : 100,
-    "damage" : 12
+    "damage" : 12,
+    "armor" : 1.2
          }
 enemy = {
     "name" : "Злой гоблин",
     "health" : 50,
-    "damage" : 24 
+    "damage" : 24,
+    "armor" : 1.2
         }
 
 print(f"{player['name'].title()} нападает на {enemy['name']}")
